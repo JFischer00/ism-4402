546ac417de4cd31cab249ca7797e221ab0ea38d6
Jordan Fischer <jordan@thefischers.org>
2020-08-31 10:36:59 -0400

    Add files via upload

---
+++ county_land.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[1]:
+
+
+import pandas as pd
+Location = "county_land.xls"
+df = pd.read_excel(Location)
+df.head()
+
+
+# In[ ]:
+
+
+
+
---
+++ nc_census.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[2]:
+
+
+import pandas as pd
+Location = "nc_census.csv"
+df = pd.read_csv(Location)
+df.head()
+
+
+# In[ ]:
+
+
+
+
